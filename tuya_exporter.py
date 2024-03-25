0156afb10c8b5f947e9082765ba5cfd645b4fe36
Poil <poil@quake.fr>
2024-03-25 11:48:47 +0100

    Fallback for T34-Smart Plug+ & update config

--- tuya_exporter.py
+++ tuya_exporter.py
@@ -6,6 +6,7 @@ from typing import List
 import click
 import tinytuya
 import attr
+import yaml
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
@@ -16,17 +17,22 @@ class DeviceConfig:
     ip: str
     device_id: str
     local_key: str
+    version: str
 
     @classmethod
     def parse(cls, s) -> "DeviceConfig":
-        name, ip, device_id, local_key = s.split(":")
+        try:
+            name, ip, device_id, local_key, version = s.split(":::")
+        except:
+            print(s.split(':::'))
         return cls(
             name=name,
             ip=ip,
             device_id=device_id,
             local_key=local_key,
+            version=version,
         )
-
+        
 
 class Collector:
     def __init__(self, configs: List[DeviceConfig]):
@@ -45,14 +51,17 @@ class Collector:
         for config in self.configs:
             d = tinytuya.OutletDevice(config.device_id, config.ip, config.local_key)
             print(f"Parsing {config}")
-            d.set_version(3.3)
+            d.set_version(float(config.version))
             d.set_socketTimeout(2)
             d.updatedps([18, 19, 20])
             data = d.status()
             if not data.get("Error"):
-                current_gauge.add_metric([config.name], float(data.get("dps", {}).get("18", 0)) / 1000.0)
-                power_gauge.add_metric([config.name],   float(data.get("dps", {}).get("19", 0)) / 10.0)
-                voltage_gauge.add_metric([config.name], float(data.get("dps", {}).get("20", 0)) / 10.0)
+                current_gauge.add_metric([config.name], float(data.get("dps", {}).get("18", data.get("dps", {}).get("21", 0))) / 1000.0)
+                power_gauge.add_metric([config.name],   float(data.get("dps", {}).get("19", data.get("dps", {}).get("22", 0))) / 10.0)
+                voltage_gauge.add_metric([config.name], float(data.get("dps", {}).get("20", data.get("dps", {}).get("23", 0))) / 10.0)
+            else:
+                print(f"Error getting data for device {config.name} {data}")
+
         yield current_gauge
         yield power_gauge
         yield voltage_gauge
